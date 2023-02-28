09046c785bd3f896560a77be3355e804d521ca6a
biggujo <gen.style22@gmail.com>
2023-02-28 20:22:01 +0200

    add main()

--- src/Point2D.java
+++ src/Point2D.java
@@ -1,3 +1,9 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+
 public class Point2D extends Point implements Comparable<Point2D> {
 
     public Point2D(double x, double y) {
@@ -12,7 +18,7 @@ public class Point2D extends Point implements Comparable<Point2D> {
 
     @Override
     public int compareTo(Point2D givenPoint) {
-        return Double.compare(this.getCoord(1), givenPoint.getCoord(1));
+        return Double.compare(this.getX(), givenPoint.getX());
     }
 
     public double getX() {
@@ -29,5 +35,27 @@ public class Point2D extends Point implements Comparable<Point2D> {
 
     public void setY(double newY) {
         setCoord(2, newY);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Point2D> point2DArrayList = new ArrayList<>();
+
+        // Set locale
+        DecimalFormatSymbols localeUS = new DecimalFormatSymbols(Locale.US);
+        DecimalFormat decimalFormat = new DecimalFormat("#.##", localeUS);
+
+        for (int i = 0; i < 5; i++) {
+            double randX = Double.parseDouble(decimalFormat.format(Math.random()));
+            double randY = Double.parseDouble(decimalFormat.format(Math.random()));
+
+            point2DArrayList.add(new Point2D(randX, randY));
+        }
+
+        System.out.println("Unsorted:");
+        point2DArrayList.forEach(System.out::println);
+
+        Collections.sort(point2DArrayList);
+        System.out.println("Sorted:");
+        point2DArrayList.forEach(System.out::println);
     }
 }
